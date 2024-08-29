45c581ba40182a1a17af27a27d1a937861aa71d8
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2024-08-30 00:13:29 +0200

    adding snappy

--- compres_testmore.py
+++ compres_testmore.py
@@ -1,17 +1,22 @@
 import time
 import zlib
 # pip install lz4
+# pip install python-snappy
+import snappy
 import lz4.frame
 import numpy as np
 from PIL import Image
 
+def compress_zlib(nd_array_bytes, level):
+    compressed_data = zlib.compress(nd_array_bytes, level=level)
+    return compressed_data
 
 def compress_lz4(nd_array_bytes, level):
     compressed_data = lz4.frame.compress(nd_array_bytes)
     return compressed_data
 
-def compress_zlib(nd_array_bytes, level):
-    compressed_data = zlib.compress(nd_array_bytes, level=level)
+def compress_snappy(nd_array_bytes, level):
+    compressed_data = snappy.compress(nd_array_bytes)
     return compressed_data
 
 def measure_compression_time(image_path, method,compression_level, iterations=100):
@@ -52,7 +57,12 @@ image_path = 'frame_1.png'  # Replace with the actual path to your image
 compression_level = 1  # You can change this to any level from 1 to 9
 
 # Measure average compression time
-average_compression_time = measure_compression_time(image_path, compress_lz4, compression_level)
-average_compression_time = measure_compression_time(image_path, compress_lz4, compression_level)
+average_compression_time = measure_compression_time(image_path, compress_zlib, compression_level)
+print(f'(ZLIB) Average compression time over 100 iterations: {average_compression_time:.6f} seconds')
 
-print(f'Average compression time over 100 iterations: {average_compression_time:.6f} seconds')
+average_compression_time = measure_compression_time(image_path, compress_lz4, compression_level)
+print(f'(LZ4) Average compression time over 100 iterations: {average_compression_time:.6f} seconds')
+
+average_compression_time = measure_compression_time(image_path, compress_snappy, compression_level)
+print(f'(SNAPPY) Average compression time over 100 iterations: {average_compression_time:.6f} seconds')
+
